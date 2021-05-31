4eabb605c4cf74075dde889ecbbdb503e95d35c4
Tarik <tarikpnr147@gmail.com>
2021-05-31 15:08:40 +0300

    feat: user auth service and login signup services
    - token service with jwt
    - axios initialized
    - login and signup
    - auth actions on client

---
+++ api/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let User;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
